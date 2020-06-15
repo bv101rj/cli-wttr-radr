1da0a4911f8e21be00d38f35ccd9c871bbc78870
bv101rj <robert@janey.tech>
2020-06-15 16:30:43 -0400

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mv radr /usr/local/bin/radr
+mv wttr /usr/local/bin/wttr
+chmod +x /usr/local/bin/radr
+chmod +x /usr/local/bin/wttr
+ls -halt /usr/local/bin/radr
+ls -halt /usr/local/bin/wttr
